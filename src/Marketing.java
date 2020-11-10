ce0889ef2dfcb32439d0a913e64873761ac6426b
Chitzuyun <73839912+Chitzuyun@users.noreply.github.com>
2020-11-10 18:27:24 +0800

    11/10 add  Class
    add  Class

---
+++ src/Marketing.java
@@ -0,0 +1,112 @@
+import java.util.Date;
+
+public class Marketing {
+	
+	private int Marketing_id;
+	private String name;
+	private Date start_date;
+	private Date end_date;
+	private String description;
+	private String Brand_no;
+	private String Brand_name;
+	private int product_Id;
+	private String product_name;
+	private String product_type;
+	private String product_category;
+	private String product_series;
+	private String product_skin;
+	private int product_price;
+	private float product_rating;
+	public int getMarketing_id() {
+		return Marketing_id;
+	}
+	public void setMarketing_id(int marketing_id) {
+		Marketing_id = marketing_id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Date getStart_date() {
+		return start_date;
+	}
+	public void setStart_date(Date start_date) {
+		this.start_date = start_date;
+	}
+	public Date getEnd_date() {
+		return end_date;
+	}
+	public void setEnd_date(Date end_date) {
+		this.end_date = end_date;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getBrand_no() {
+		return Brand_no;
+	}
+	public void setBrand_no(String brand_no) {
+		Brand_no = brand_no;
+	}
+	public String getBrand_name() {
+		return Brand_name;
+	}
+	public void setBrand_name(String brand_name) {
+		Brand_name = brand_name;
+	}
+	public int getProduct_Id() {
+		return product_Id;
+	}
+	public void setProduct_Id(int product_Id) {
+		this.product_Id = product_Id;
+	}
+	public String getProduct_name() {
+		return product_name;
+	}
+	public void setProduct_name(String product_name) {
+		this.product_name = product_name;
+	}
+	public String getProduct_type() {
+		return product_type;
+	}
+	public void setProduct_type(String product_type) {
+		this.product_type = product_type;
+	}
+	public String getProduct_category() {
+		return product_category;
+	}
+	public void setProduct_category(String product_category) {
+		this.product_category = product_category;
+	}
+	public String getProduct_series() {
+		return product_series;
+	}
+	public void setProduct_series(String product_series) {
+		this.product_series = product_series;
+	}
+	public String getProduct_skin() {
+		return product_skin;
+	}
+	public void setProduct_skin(String product_skin) {
+		this.product_skin = product_skin;
+	}
+	public int getProduct_price() {
+		return product_price;
+	}
+	public void setProduct_price(int product_price) {
+		this.product_price = product_price;
+	}
+	public float getProduct_rating() {
+		return product_rating;
+	}
+	public void setProduct_rating(float product_rating) {
+		this.product_rating = product_rating;
+	}
+	
+	
+}
---
+++ src/Member.java
@@ -0,0 +1,63 @@
+import java.util.Date;
+
+public class Member {
+	
+	
+	private String name;
+	private String account_id;
+	private String password;
+	private String address;
+	private int phone;
+	private Date birth;
+	private String email;
+	private String gender;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAccount_id() {
+		return account_id;
+	}
+	public void setAccount_id(String account_id) {
+		this.account_id = account_id;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public int getPhone() {
+		return phone;
+	}
+	public void setPhone(int phone) {
+		this.phone = phone;
+	}
+	public Date getBirth() {
+		return birth;
+	}
+	public void setBirth(Date birth) {
+		this.birth = birth;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+}
